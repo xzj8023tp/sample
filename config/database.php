6732d07bce63bfda40b2d759c79a423242127d27
夏赵建 <xzj8023tp@163.com>
2018-12-26 07:20:18 +0000

    updated database configuration

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+$db_config = get_db_config();
 
 return [
 
@@ -13,7 +14,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    //'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
